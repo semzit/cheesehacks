7013f7b83e89d6425a32d56b53c1a5e14aebf9d0
VasantA2730 <vasantarul12@gmail.com>
2026-02-28 22:18:29 -0600

    Looping over images in the file, and then getting the objects within all of them

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,17 +9,27 @@ const client = new vision.ImageAnnotatorClient();
 /**
  * TODO(developer): Uncomment the following line before running the sample.
  */
+try {
+  const files = fs.readdirSync("img");
 
-const fileName = path.join("img", "test.jpg");
-const request = {
-  image: { content: fs.readFileSync(fileName) },
-};
+  files.forEach(async (fileName: any) => {
+    console.log(fileName);
 
-const [result] = await client.objectLocalization(request);
-const objects: Array<any> = result.localizedObjectAnnotations;
-objects.forEach((object) => {
-  console.log(`Name: ${object.name}`);
-  console.log(`Confidence: ${object.score}`);
-  const vertices: Array<any> = object.boundingPoly.normalizedVertices;
-  vertices.forEach((v) => console.log(`x: ${v.x}, y:${v.y}`));
-});
+    fileName = path.join("img", fileName);
+
+    const request = {
+      image: { content: fs.readFileSync(fileName) },
+    };
+
+    const [result] = await client.objectLocalization(request);
+    const objects: Array<any> = result.localizedObjectAnnotations;
+    objects.forEach((object) => {
+      console.log(`Name: ${object.name}`);
+      console.log(`Confidence: ${object.score}`);
+      const vertices: Array<any> = object.boundingPoly.normalizedVertices;
+      vertices.forEach((v) => console.log(`x: ${v.x}, y:${v.y}`));
+    });
+  });
+} catch (err) {
+  console.error("Could not list directory", err);
+}
